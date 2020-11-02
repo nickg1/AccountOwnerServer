146a567aa5f0bd3b2c62a008333a331c8113c3dc
Nicholas Gibson <nckgibson4@gmail.com>
2020-11-02 12:47:15 -0800

    first

---
+++ AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Account, AccountDto>();
+            CreateMap<OwnerForCreationDto, Owner>();
+            CreateMap<OwnerForUpdateDto, Owner>();
+
+        }
+    }
+}
---
+++ Contracts/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
+{
+    public interface IAccountRepository 
+    {
+        IEnumerable<Account> AccountsByOwner(Guid ownerId);
+
+    }
+}
